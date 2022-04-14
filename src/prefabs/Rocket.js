805a1346eae6120015e7b685460d64831ff95f9b
Andres Benitez <spartansup18@gmail.com>
2022-04-13 23:53:20 -0700

    first commit

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,46 @@
+// Rocket prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+      super(scene, x, y, texture, frame);
+  
+      // add object to existing scene
+      scene.add.existing(this);
+      this.isFiring = false;
+      this.moveSpeed = 2;
+      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
+      this.moveleft;
+      this.moveright;
+      this.firebutton;
+    }
+
+    update(){
+        //left/right movement
+        if(!this.isFiring){
+            if(this.moveleft.isDown && this.x >= borderUISize + this.width){ 
+                this.x -= this.moveSpeed;
+            }  else if(this.moveright.isDown && this.x <= game.config.width -
+            borderUISize - this.width){
+                this.x += this.moveSpeed;
+            }
+        }
+        //fire button
+        if (Phaser.Input.Keyboard.JustDown(this.firebutton) && !this.isFiring) {
+            this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
+          }
+        // if fired, move up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
+            this.y -= this.moveSpeed;
+        }
+
+        //reset on miss
+        if ( this.y <= borderUISize * 3 + borderPadding){
+            this.reset();
+        }
+    }
+
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
+}

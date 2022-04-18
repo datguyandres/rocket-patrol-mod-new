2698a44c6682cc83d818cf624fb42ee18f26d72c
Andres Benitez <spartansup18@gmail.com>
2022-04-18 13:42:20 -0700

    added comments

--- src/main.js
+++ src/main.js
@@ -12,3 +12,11 @@ let keyF, keyR, keyLEFT, keyRIGHT;
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+/* simultaneous two-player mode added
+redesigned the game's artwork, sound and UI and changed the theme and aesthetic to
+shark hunting instead of shotting spaceships
+lastly added a timer which should then make a grand total of 100 points :)
+the project took me about 10 ish hours to complete entirely.
+
+Andres Benitez, SHARK FISHING, 4/18/2022*/
